b46c0bb6ac8ddb133a73f560f0619abc1df80bd3
ramanjiazuredevops <89173707+ramanjiazuredevops@users.noreply.github.com>
2022-09-20 14:52:08 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "demo" {
 					ami 				      =     "ami-0a531e5afd4d5f3ad"
 					instance_type		      =     "t2.micro"
 					monitoring			      =     "false"
-					key_name			      =     "gel"
+					key_name			      =     "junedevops"
 					subnet_id			      =     "subnet-006956d784051a51c"
 					associate_public_ip_address   =     "true"
 					source_dest_check		      =     "true"
