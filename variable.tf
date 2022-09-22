988938b2740564b930b82910b48fa0b185f9f95f
ramanjiazuredevops <89173707+ramanjiazuredevops@users.noreply.github.com>
2022-09-22 12:52:18 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,6 +1,5 @@
 provider "aws" {
-	region		="ap-south-1"
-	access_key		="AKIATMELVKS76NWQ3EXG"
-	secret_key		="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"
-	}
-
+        region          ="us-west-2"
+        access_key              ="AKIATMELVKS76NWQ3EXG"
+        secret_key              ="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"
+        }
